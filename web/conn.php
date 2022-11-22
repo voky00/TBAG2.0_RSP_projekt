1e6b3499cbaa42fb2e9536b6e4e776b93e469d3e
voky00 <vokoun.tomas@mujmail.cz>
2022-11-22 15:10:12 +0100

    drobné úpravy

---
+++ web/conn.php
@@ -0,0 +1,8 @@
+<?php                                                                                  
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "tbag";
+$spojeni = mysqli_connect($servername, $username, $password, $dbname);
+ mysqli_set_charset($spojeni, "utf8");
+?>
